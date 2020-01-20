6c3b2028e0d8970f57853ef677ad9bf5010e552f
Stuart Jansen <sjansen@buscaluz.org>
2020-01-20 06:33:14 -0700

    upgrade dependencies

--- src/environment.ts
+++ src/environment.ts
@@ -1,18 +1,8 @@
-const dev = {
-    clientId: process.env.REACT_APP_CLIENT_ID || '511828570984-7nmej36h9j2tebiqmpqh835naet4vci4.apps.googleusercontent.com',
-    openIdConnectUrl: process.env.REACT_APP_OIDC_URL || 'https://accounts.google.com/',
-    redirectURL: process.env.REACT_APP_REDIRECT_URL || 'http://localhost:8000/callback',
-};
-
-const prod = {
+const config = {
     clientId: process.env.REACT_APP_CLIENT_ID || 'MISSING',
     openIdConnectUrl: process.env.REACT_APP_OIDC_URL || 'MISSING',
     redirectURL: process.env.REACT_APP_REDIRECT_URL || 'MISSING',
 };
-
-const config = process.env.REACT_APP_STAGE === 'production'
-    ? prod
-    : dev;
 
 export default {
     ...config
